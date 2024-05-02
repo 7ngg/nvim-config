41868c60cb1fd18dc2f931fb5a49bf242d826bbc
tng <m.farm4nov@gmail.com>
2024-05-02 16:24:20 +0400

    sass: removed map file

--- lua/tng_lazy/remap.lua
+++ lua/tng_lazy/remap.lua
@@ -24,7 +24,7 @@ vim.keymap.set("n", "<leader>sp", function ()
     local current_buffer = vim.api.nvim_get_current_buf()
     local filename = vim.api.nvim_buf_get_name(current_buffer)
     local new_filename = filename:gsub("%.scss$", ".min.css")
-    local command = string.format("sass %s %s --style compressed", filename, new_filename)
+    local command = string.format("sass --no-source-map %s %s --style compressed", filename, new_filename)
 
     local ok, err = os.execute(command)
 
