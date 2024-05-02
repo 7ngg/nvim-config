94f63c2bcf5281b2d206745488edc2414415e485
tng <m.farm4nov@gmail.com>
2024-05-02 19:51:40 +0400

    files update

--- lua/tng_lazy/lazy/treesitter.lua
+++ lua/tng_lazy/lazy/treesitter.lua
@@ -4,7 +4,7 @@ return ({{
 
     config = function()
         require('nvim-treesitter.configs').setup({
-            --ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
             sync_install = false,
             auto_install = true,
             indent = {
